a8cd126fe60e7712d0326a8ce8e36bffd6ab1e8a
Rodrigo Borges <rodrigoesborges@gmail.com>
2016-08-21 18:08:06 -0300

    altera script para utilizar o rds gerado pelo outro script de consolidação de despesas

--- reclassifica-gastos.R
+++ reclassifica-gastos.R
@@ -12,15 +12,11 @@ tradutor <- tradutor %>%
   mutate(codigo = substr(codigo7,1,5))
 
 tradutor2 <- tradutor[!duplicated(tradutor$codigo),2:5]
-# Carrega tabela que POF com caderneta de gastos e codigos dos produtos na POF
 
-load(file = "2009/t_caderneta_despesa_s.rda")
-t_caderneta_despesa_s <- t_caderneta_despesa_s %>% 
-  mutate(codigo = substr(paste0(prod_num_quadro_grupo_pro,cod_item), 1, 5),
-         despmes = (valor_anual_expandido2/fator_expansao2)/12 ,
-         cod.uc = paste0(cod_uf, num_seq, num_dv, cod_domc, num_uc))
+# Carrega tabela com todas as despesas consolidadas gerada da POF
+todas_despesas<-readRDS("todas-despesas-domicilios.rds")
 
 
-despesas_mensais <- t_caderneta_despesa_s[,c('cod.uc','codigo','despmes')]
+despesas_mensais <- todas_despesas[,c('cod.uc','codigo','despmes')]
 
 gastos_SCN <- left_join(despesas_mensais, tradutor2)
