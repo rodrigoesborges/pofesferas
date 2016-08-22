d82b860945f617184c361e5ad2ff0a94d089d452
Rodrigo Borges <rodrigoesborges@gmail.com>
2016-08-22 15:01:20 -0300

    conserta erro de codificação ao ler csv de origens distintas.

--- scripts/gastos-esferas.R
+++ scripts/gastos-esferas.R
@@ -31,12 +31,11 @@ tradutor <- read_excel(dir(recursive = TRUE)[grep(pattern = "Tradutor_POF",x = d
                        sheet = 1 , skip = 1)
 
 # Tabela de componentes hierarquizada cod68 x cod 20 - dicionário de tradução agregado
-options( encoding = "utf8" )
 componentes <- read.csv("tradutores/cod68X20componentes-HIERARQ.csv", 
-                        colClasses = c("item68x20" = "character","cod68" = "character"))
+                        colClasses = c("item68x20" = "character","cod68" = "character"), encoding = "utf8")
 
 # Carrega tabela com códigos POF que não entram inicialmente como Consumo Final das Famílias
-pofnaoconsumo <- read.csv("tradutores/codigos_semtradutor.csv", stringsAsFactors = FALSE, colClasses = c("x" = "character"))
+pofnaoconsumo <- read.csv("tradutores/codigos_semtradutor.csv", stringsAsFactors = FALSE, colClasses = c("x" = "character") , encoding = "latin1")
 
 # Definimos função para recodificar, recalcular e selecionar apenas dados necessários para as próximas fases
 recod.despesas <- function (tabela = t_despesa_individual_s,
